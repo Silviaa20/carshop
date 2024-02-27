3d6dc5409949c7b357b74ec3d88afa6e374e037f
bespo <b.esposito@contrader.it>
2024-02-27 16:32:30 +0100

    prodotto model e dto

---
+++ src/main/java/it/contrader/carshop/dto/Prodotto.java
@@ -0,0 +1,19 @@
+package it.contrader.carshop.dto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Prodotto {
+    private Long id;
+    private String marchio;
+    private String modello;
+    private Double prezzo;
+    private Long quantita;
+
+}
